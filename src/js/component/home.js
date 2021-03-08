2a9acca076c008212552619487d1276466e9c42c
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2021-03-08 01:06:31 +0000

    Fin

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,56 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { Fragment, useState } from "react";
 
 //create your first component
-export function Home() {
+const Home = () => {
+	const [color, setColor] = useState("red");
+
+	const handleClick = e => {
+		const origen = e.target.id;
+		setColor(origen);
+	};
+
+	const getSelected = (classes, origen) => {
+		if (origen === color) {
+			return classes + " selected";
+		} else {
+			return classes;
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="trafficTop"></div>
+			<div className="lights">
+				<div
+					id="red"
+					className={getSelected(
+						"bg-danger rounded-circle light",
+						"red"
+					)}
+					onClick={e => {
+						handleClick(e);
+					}}></div>
+				<div
+					id="yellow"
+					className={getSelected(
+						"bg-warning rounded-circle light",
+						"yellow"
+					)}
+					onClick={e => {
+						handleClick(e);
+					}}></div>
+				<div
+					id="green"
+					className={getSelected(
+						"bg-success rounded-circle light",
+						"green"
+					)}
+					onClick={e => {
+						handleClick(e);
+					}}></div>
+			</div>
 		</div>
 	);
-}
+};
+
+export default Home;
